a3316a25048e322565809cc7eb40cea49ee9a3e4
july <mendozajulietabelen87@gmail.com>
2025-05-31 16:24:40 -0300

    hice el proyecto instalable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=['flaskr'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
