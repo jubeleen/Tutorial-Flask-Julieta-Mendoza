c03a6aaa617693517c0fe7184356614daec2dda7
july <mendozajulietabelen87@gmail.com>
2025-05-31 16:05:23 -0300

    realice las plantillas

--- flaskr/db.py
+++ flaskr/db.py
@@ -21,13 +21,13 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
-
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
-        
+
+
 @click.command('init-db')
 def init_db_command():
     """Clear the existing data and create new tables."""
